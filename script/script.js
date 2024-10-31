cfe0d0007b6458395bb0fa2729a4fcf9c2b483af
Ankan <ankanghorai30@gmail.com>
2024-10-31 14:38:43 +0530

    Live Style

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// ------------------- typing animation -----------------------------
+
+const typed = new Typed('.typing', {
+    strings: [""," Full-Stack Developer", " passionate designer", " creative thinker"," gamer and streamer"," YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
